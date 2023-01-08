e831ad72ec1be57b9caca024c348eaae5a1fc235
Oliver Chang <=>
2023-01-08 18:43:24 -0500

    created checkIfWeekend and formatDate function

--- src/app/get-stock-list/get-stock-list.component.ts
+++ src/app/get-stock-list/get-stock-list.component.ts
@@ -10,15 +10,17 @@ import { StocksService } from '../stocks.service';
 
 export class GetStockListComponent {
   button = document.getElementById('button1');
-  API_TOKEN: string = 'be8c9784e7cd1f96be661df808c6be2b6eed78d6';
+  tiingo_API_TOKEN: string = 'be8c9784e7cd1f96be661df808c6be2b6eed78d6';
   request = require('request');
   ticker: string = '';
   date: string = '';
-  // url: string = `https://api.tiingo.com/tiingo/daily/${this.ticker}/prices?startDate=${this.date}&token=${this.API_TOKEN}`;
+  showTicker!: string;
   jsonData: any;
-  displayMsg = '';
+  displayMsg: string | undefined = '';
   openPrice: any;
   closePrice: any;
+  unformattedDate: string = '';
+  showDate: string = '';
   // date = this.jsonData[0].date;
 
   constructor(private router: Router, private stocksService: StocksService) {
@@ -33,6 +35,44 @@ export class GetStockListComponent {
       this.jsonData = data;
       this.closePrice = this.jsonData[0].close;
       this.openPrice = this.jsonData[0].open;
+      this.unformattedDate = this.jsonData[0].date;
+      console.log(`unformatted date: ` + this.unformattedDate);
+      this.showDate = `${this.formatDate(this.unformattedDate)}`;
+      this.displayMsg = this.displayMessage();
     });
+    this.showTicker = this.ticker.toUpperCase();
+  }
+
+  displayMessage() {
+    console.log(`Formatted date: ` + this.showDate);
+    const day = new Date(this.showDate);
+    console.log(`day: ` + day);
+    let dayOf = day.getDay();
+    console.log(dayOf);
+    console.log(this.checkIfWeekend());
+    this.displayMsg = this.checkIfWeekend();
+    return `Successfully received data`;
+  }
+
+  formatDate(date: string){
+    const splitDate = date.split("T")[0];
+    console.log(splitDate);
+    // convert to number to subtract 1 day, then re-convert to string
+    // let day = (parseInt(splitDate[2])-1).toString();
+    // replace updated day in date
+    // splitDate[2] = day;
+    // const finalDate = splitDate.join("-");
+    console.log(`Date formatted: ${splitDate}`);
+    return splitDate;
+  }
+
+  checkIfWeekend(){
+    const inputDate = new Date(this.date);
+    const day = inputDate.getDay();
+
+    if(day === 0 || day === 6){
+      return `Stock market is not open on the weekends.`;
+    }
+    return `Checking day of the week..`;
   }
 }
